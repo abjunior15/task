89b3535354d6e23eba75ba4a10d8f91c77398f13
Abdullah Tariq <131231843+abjunior15@users.noreply.github.com>
2025-10-19 15:30:44 +0500

    .
    removed the unused route

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,7 +4,6 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import '../features/startup/view/splash_view.dart';
 
 const String defaultRoute = '/';
-const String onboardingRoute = '/on-boarding-view';
 const String homeRoute = '/Home-view';
 
 final GlobalKey<NavigatorState> kAppNavigatorKey = GlobalKey<NavigatorState>();
